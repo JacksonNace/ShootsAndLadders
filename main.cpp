f8690b99302fcd80b6014f32e189d171ca67c31c
Jackson Nace <jacksonnace964@gmail.com>
2025-02-25 21:06:37 -0600

    add hay's files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,60 @@
-#include "Board.h"
+#include "board.h"
+#include "player.h"
+#include <string>
+#include <iostream>
+#include <ctime>
+#include <random>
 
-int main () {
-  
-  return 0;
-}
+int main()
+    {
+    Board board;//Constructs the board.
+    std::string p1 = "Player1";
+    Player player1(p1, board);//Constructs player 1 and passes the board object.
+    std::string p2 = "Player2";
+    Player player2(p2,board);//Constructs player 2 and passes the board object.
+
+    std::cout << "              Snakes & Ladders" << std::endl;
+    std::cout << "____________________________________________" << std::endl;
+
+    srand(time(0));
+
+    while(player1.isFinished() == false && player2.isFinished() == false)
+        {
+        std::cout << "Press R to roll" << std::endl;
+
+        /*
+        I need to implement a pause in the loop to allow for players to roll.
+        This will likely take time.
+        */
+        std::string input = "";
+        while(input != "R" && input != "r")
+            {
+            std::cin >> input;
+            }
+
+        unsigned int roll1 = rand() % 6 + 1;
+        unsigned int roll2 = rand() % 6 + 1;
+
+        player1.move(roll1);
+        player2.move(roll2);
+        }
+
+    if(player1.isFinished() == true && player2.isFinished() == true)
+        {
+        std::cout << "It's a tie." << std::endl;
+        }
+    else if(player1.isFinished() == true)
+        {
+        std::cout << p1 << " is the winner." << std::endl;
+        }
+    else if(player2.isFinished() == true)
+        {
+        std::cout << p2 << " is the winner." << std::endl;
+        }
+
+    player1.printStats();
+    player2.printStats();
+
+    //I need to create deconstructors for player and board.
+
+    }
---
+++ player.cpp
@@ -0,0 +1,94 @@
+#include <string>
+#include <iostream>
+#include "board.h"
+#include "player.h"
+
+Player::Player(std::string pName, Board board)
+    {
+    pos = board.getHead();
+    name = pName;
+    sStat = 0;
+    lStat = 0;
+    mStat = 0;
+    tStat = 0;
+    fin = false;
+
+    }
+
+void Player::tileType()
+    {
+    if(Board::isSpecial(pos))
+        {
+        unsigned int start = pos->pos;
+        unsigned int end = pos->movement->pos;
+        if(start > end)
+            {
+            printSnakeHit(start,end);
+            tStat += start - end;
+            sStat++;
+            pos = pos->movement;
+            }
+        else if(end > start)
+            {
+            printLadderHit(start,end);
+            tStat += end - start;
+            lStat++;
+            pos = pos->movement;
+            }
+        }
+    }
+
+void Player::move(unsigned int roll)
+    {
+    if(roll == 1)
+        {
+        std::cout << name + " rolled an " << roll << "." << std::endl;
+        }
+    else//Grammer    
+        {
+        std::cout << name + " rolled a " << roll << "." << std::endl;
+        }
+
+    mStat++;
+    while(pos->next != nullptr && roll > 0)
+        {
+        pos = pos->next;
+        roll--;
+        tStat++;
+        }
+    
+    if(pos->next == nullptr)//Checks if it is at the end
+        {
+        fin = true;
+        }
+
+    tileType();
+
+    std::cout << name + " is now on tile " << pos->pos << "." << std::endl;
+    }
+
+ void Player::printSnakeHit(unsigned int start, unsigned int end)
+    {
+    std::cout << name + " landed on a snake. They have moved from tile" << start << " to tile" << end <<"." << std::endl;
+    }
+
+ void Player::printLadderHit(unsigned int start, unsigned int end)
+    {
+    std::cout << name + " landed on a ladder. They have moved from tile" << start << " to tile" << end <<"." << std::endl;
+    }
+
+bool Player::isFinished()
+    {
+    return fin;
+    }
+
+void Player::printStats()
+    {
+    std::cout << "[" + name + "'s Stats || Snakes Hit: " << sStat << " || Ladders Hit: " << lStat << " || Number of Roles: " << mStat << " || Total Spaces Moved: " << tStat << "]" << std::endl;
+    }
+
+Player::~Player()
+    {
+    pos = nullptr;
+    delete pos;
+    }
